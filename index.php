9b3d80ed1265e464edc43f5a79e63a87e41057d6
Sonia Segovia <segoviasonia267@gmail.com>
2022-02-19 11:10:40 -0300

    nuevos cambios

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>PROYECTOS</title>
-    
+    <?php require_once "menu.php";?>
   </head>
   <body>
       <div class="container">
 <div class="row justify-content-center">
 <div class="col-6 p-5 bg-white shadow-lg rounded">
-    <h3> NUEVO PRODUCTO con POST</h3>
+    <h3> NUEVO PRODUCTO CON POST</h3>
     <hr>
     <form method="post" action="recibe_post.php">
         <div class="form-group">
